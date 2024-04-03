bf1445cf0c2434b072ee97efc066b19f86cf9950
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2024-04-03 14:37:54 +0200

    Final

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = 
+      `<h2>Certificate of Achievement</h2>
+      <p>This is to certify that</p>
       <h3>${studentName}</h3>
+      <p>has almost completed</p>
+      <h3>${courseName}</h3>
+      <p>${personalMessage}🏆</p>
+      <img src="logo.png" width="max-width" height="70px">
+      <P>I am the greatest 😼</p>
     `;
     
       //  Display the modal
@@ -39,8 +46,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+   closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
+  
   });
   
